94e860f348bdaea3b19ec9f79c6e444614e2a049
LAPTOP-BLP8O8RL\Yash <pyash7224@gmail.com>
2024-05-29 14:17:49 -0400

    Update Resume Link

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "YASH PATEL",
   profile: '/profile.png',
   designation: "Full-Stack Developer",
-  description: "I'm Yash Patel, a full-stack developer with over 2 years of hands-on experience, specializing in Java and web development. Proficient in backend technologies such as Spring Boot and Hibernate, I've cultivated strong problem-solving skills and effective communication throughout my software development journey. This background enables me to collaborate seamlessly across diverse teams. Thriving in dynamic environments, I leverage my expertise to create impactful solutions. Currently, I am actively seeking exciting job opportunities that allow me to contribute my skills and drive meaningful change in the tech industry.",
+  description: "I'm Yash Patel, a full-stack developer with around 3 years of hands-on experience, specializing in Java and web development. Proficient in backend technologies such as Spring Boot and Hibernate, I've cultivated strong problem-solving skills and effective communication throughout my software development journey. This background enables me to collaborate seamlessly across diverse teams. Thriving in dynamic environments, I leverage my expertise to create impactful solutions. Currently, I am actively seeking exciting job opportunities that allow me to contribute my skills and drive meaningful change in the tech industry.",
   email: 'yashpatel9805@gmail.com',
   phone: '+1 365-366-5932',
   address: 'Hamilton, ON, L9A 0C5',
